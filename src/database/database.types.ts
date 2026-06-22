45477a1f1f04e62eae64f2b141af411f10ea5303
keliumJU <keliumju@gmail.com>
2026-06-22 14:48:29 -0500

    Add fields to handle password and jwt token in users table

--- src/database/database.types.ts
+++ src/database/database.types.ts
@@ -3,6 +3,8 @@ import { ColumnType, Generated } from 'kysely';
 export interface UsersTable {
   id: Generated<string>;
   email: string;
+  password_hash: string;
+  token_version: number;
   created_at: ColumnType<Date, string | undefined, never>;
 }
 
