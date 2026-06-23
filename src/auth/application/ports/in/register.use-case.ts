4adcc69d7d1a65841add7a48d4f3236529f24b35
keliumJU <keliumju@gmail.com>
2026-06-22 22:47:33 -0500

    Add ApiProperty decorator to have an input in swagger docs

--- src/auth/application/ports/in/register.use-case.ts
+++ src/auth/application/ports/in/register.use-case.ts
@@ -1,5 +1,10 @@
-export interface RegisterInput {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class RegisterInput {
+  @ApiProperty()
   email: string;
+
+  @ApiProperty()
   password: string;
 }
 
