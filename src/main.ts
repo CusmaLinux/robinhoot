45c9f350e1666b5c95b6e06ec5954dded9c4fcf1
keliumJU <keliumju@gmail.com>
2026-06-22 22:47:58 -0500

    Add the wrapper to swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -9,7 +10,20 @@ async function bootstrap() {
       'JWT_SECRET environment variable must be set and at least 32 characters long',
     );
   }
+
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Robinhoot')
+    .setDescription(
+      'Create educational exercises and practice with your students—all in real time. Have fun!',
+    )
+    .setVersion('0.1')
+    .addTag('robinhoot')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
